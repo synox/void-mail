b6a95646666c6fe6c93970be656b8afdc153e09f
synox <synox@user.github.org>
2019-03-23 08:14:45 +0700

    refactor notif, moved scripts to head

--- public/javascripts/notifications.js
+++ public/javascripts/notifications.js
@@ -1,47 +1,54 @@
-function enableNewMessageNotifications(address, reloadPage) {
-  enableNotifications()
-  const socket = io()
-  socket.emit('sign in', address)
+function showNewMailsNotification(address, reloadPage) {
+    // We want the page to be reloaded. But then when clicking the notification, it can not find the tab and will open a new one.
 
-  socket.on('reconnect', () => {
-    socket.emit('sign in', address)
-  })
-  socket.on('new emails', () => {
-    const notif = new Notification(
-      address, {
-        body: 'You have new messages',
-        icon: '/images/logo.gif'
-      })
-    notif.addEventListener('click', event => {
-      window.focus()
-      notif.close()
-    })
+    const notification = new Notification(
+        address, {
+            body: 'You have new messages',
+            icon: '/images/logo.gif',
+            tag: 'voidmail-replace-notification'
+        })
+    notification.onclick = function(event) {
+        // TODO: does not work after reloading the page
+        event.preventDefault();
+    }
 
     if (reloadPage) {
-      location.reload()
+        location.reload()
     }
-  })
+}
+
+function enableNewMessageNotifications(address, reloadPage) {
+    enableNotifications()
+    const socket = io()
+    socket.emit('sign in', address)
+
+    socket.on('reconnect', () => {
+        socket.emit('sign in', address)
+    })
+    socket.on('new emails', () => {
+        showNewMailsNotification(address, reloadPage);
+    })
 }
 
 function enableNotifications() {
-  // Let's check if the browser supports notifications
-  if (!('Notification' in window)) {
-    return false
-  }
+    // Let's check if the browser supports notifications
+    if (!('Notification' in window)) {
+        return false
+    }
 
-  // Let's check whether notification permissions have already been granted
-  if (Notification.permission === 'granted') {
-    return true
-  }
+    // Let's check whether notification permissions have already been granted
+    if (Notification.permission === 'granted') {
+        return true
+    }
 
-  // Otherwise, we need to ask the user for permission
-  if (Notification.permission !== 'denied') {
-    Notification.requestPermission(permission => {
-      // If the user accepts, let's create a notification
-      return permission === 'granted';
-    })
-  }
+    // Otherwise, we need to ask the user for permission
+    if (Notification.permission !== 'denied') {
+        Notification.requestPermission(permission => {
+            // If the user accepts, let's create a notification
+            return permission === 'granted';
+        })
+    }
 
-  // Finally, if the user has denied notifications and you
-  // want to be respectful there is no need to bother them any more.
+    // Finally, if the user has denied notifications and you
+    // want to be respectful there is no need to bother them any more.
 }
